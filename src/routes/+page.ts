c3019f87f81e74b0b73854d8a63caea99b91ddb9
Eesha Ulhaq <eesha@Eeshas-MacBook-Air.local>
2023-11-15 16:05:05 -0500

    set prerender to false for npm run build

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,4 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
+export const prerender = false; 
+//eesha set to false b/c otherwise npm build errors for the pages not built yet, maybe comeback and find better solution
